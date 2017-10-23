72f79a0c77c679e30ab9c74c29040f93b4ffd84d
Aditya Kalra <aditya_kalra@ymail.com>
2017-10-23 14:09:59 +1100

    confluence

---
+++ TestRunner/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConfluenceClient.Confluence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Page page = new Page("https://conflu.test.be", 23760731);
+
+            //page.body.storage.UpdateMacroContent("chart", "<table><tbody><tr><th>Hour</th><th>Errors</th></tr><tr><td>00</td><td>5</td></tr><tr><td>01</td><td>18</td></tr><tr><td>02</td><td>25</td></tr><tr><td>03</td><td>6</td></tr><tr><td>04</td><td>66</td></tr><tr><td>05</td><td>16</td></tr></tbody></table>");
+            page.body.storage.value += "Some new appended content";
+
+            page.Update();
+
+        }
+    }
+}
